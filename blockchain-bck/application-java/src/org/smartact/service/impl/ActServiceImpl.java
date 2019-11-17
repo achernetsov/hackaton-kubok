80e5f082017841704d8d15b15ec7c585113b8979
nchernetsov <n.chernetsov86@gmail.com>
2019-11-17 13:37:17 +0300

    file system paths fix

--- blockchain-bck/application-java/src/org/smartact/service/impl/ActServiceImpl.java
+++ blockchain-bck/application-java/src/org/smartact/service/impl/ActServiceImpl.java
@@ -5,33 +5,37 @@ import org.papernet.CompletionAct;
 import org.smartact.service.ActService;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.TimeoutException;
 import java.util.stream.Collectors;
 
 @Service
 public class ActServiceImpl implements ActService {
+
     private static final String CONTRACT_ISSUE_METHOD = "issue";
+
+    private static final String CONTACT_NAME = "papercontract";
+
+    private static final String MAGNETOCORP_PATH = "/home/nchernetsov/go/src/github.com/hyperledger/fabric-samples/commercial-paper/organization/magnetocorp/";
 
     @Override
     public CompletionAct issue(CompletionAct act) {
         Gateway.Builder builder = Gateway.createBuilder();
 
-        String contractName = act.getName();
-
         try {
             // A wallet stores a collection of identities
-            Path walletPath = Paths.get("..", "identity", "user", "isabella", "wallet");
+            Path walletPath = Paths.get(MAGNETOCORP_PATH, "identity", "user", "isabella", "wallet");
             Wallet wallet = Wallet.createFileSystemWallet(walletPath);
 
             // TODO
             String userName = "User1@org1.example.com";
 
-            Path connectionProfile = Paths.get("..", "gateway", "networkConnection.yaml");
+            Path connectionProfile = Paths.get(MAGNETOCORP_PATH, "gateway", "networkConnection.yaml");
 
             // Set connection options on the gateway builder
             builder.identity(wallet, userName).networkConfig(connectionProfile).discovery(false);
@@ -44,7 +48,7 @@ public class ActServiceImpl implements ActService {
 
                 // Get addressability to commercial paper contract
                 System.out.println("Use org.papernet.commercialpaper smart contract.");
-                Contract contract = network.getContract(act.getName(), "org.papernet.commercialpaper");
+                Contract contract = network.getContract(CONTACT_NAME, "org.papernet.commercialpaper");
 
                 // Issue commercial paper
                 System.out.println("Submit commercial paper issue transaction.");
@@ -57,8 +61,8 @@ public class ActServiceImpl implements ActService {
                         act.getCustomer(),
                         act.getContractNum(),
                         act.getSLA().toString(),
-                        act.getMoneyAmountPlan().toString(),
-                        act.getMoneyAmountFact().toString());
+                        act.getMoneyAmountPlan() != null ? act.getMoneyAmountPlan().toString() : "",
+                        act.getMoneyAmountFact() != null ? act.getMoneyAmountFact().toString() : "");
 
                 // Process response
                 System.out.println("Process issue transaction response.");
@@ -72,18 +76,16 @@ public class ActServiceImpl implements ActService {
 
     @Override
     public CompletionAct get(String uuid) {
-        String contractName = "papercontract";
-
         Gateway.Builder builder = Gateway.createBuilder();
 
         try {
             // A wallet stores a collection of identities
-            Path walletPath = Paths.get("..", "identity", "user", "isabella", "wallet");
+            Path walletPath = Paths.get(MAGNETOCORP_PATH, "identity", "user", "isabella", "wallet");
             Wallet wallet = Wallet.createFileSystemWallet(walletPath);
 
             String userName = "User1@org1.example.com";
 
-            Path connectionProfile = Paths.get("..", "gateway", "networkConnection.yaml");
+            Path connectionProfile = Paths.get(MAGNETOCORP_PATH, "gateway", "networkConnection.yaml");
 
             // Set connection options on the gateway builder
             builder.identity(wallet, userName).networkConfig(connectionProfile).discovery(false);
@@ -96,7 +98,7 @@ public class ActServiceImpl implements ActService {
 
                 // Get addressability to commercial paper contract
                 System.out.println("Use org.papernet.commercialpaper smart contract.");
-                Contract contract = network.getContract(contractName, "org.papernet.commercialpaper");
+                Contract contract = network.getContract(CONTACT_NAME, "org.papernet.commercialpaper");
 
                 byte[] response = contract.evaluateTransaction("queryAct", uuid);
 
@@ -110,18 +112,16 @@ public class ActServiceImpl implements ActService {
 
     @Override
     public List<CompletionAct> getActs() {
-        String contractName = "papercontract";
-
         Gateway.Builder builder = Gateway.createBuilder();
 
         try {
             // A wallet stores a collection of identities
-            Path walletPath = Paths.get("..", "identity", "user", "isabella", "wallet");
+            Path walletPath = Paths.get(MAGNETOCORP_PATH, "identity", "user", "isabella", "wallet");
             Wallet wallet = Wallet.createFileSystemWallet(walletPath);
 
             String userName = "User1@org1.example.com";
 
-            Path connectionProfile = Paths.get("..", "gateway", "networkConnection.yaml");
+            Path connectionProfile = Paths.get(MAGNETOCORP_PATH, "gateway", "networkConnection.yaml");
 
             // Set connection options on the gateway builder
             builder.identity(wallet, userName).networkConfig(connectionProfile).discovery(false);
@@ -134,7 +134,7 @@ public class ActServiceImpl implements ActService {
 
                 // Get addressability to commercial paper contract
                 System.out.println("Use org.papernet.commercialpaper smart contract.");
-                Contract contract = network.getContract(contractName, "org.papernet.commercialpaper");
+                Contract contract = network.getContract(CONTACT_NAME, "org.papernet.commercialpaper");
 
                 byte[] response = contract.evaluateTransaction("queryAllActs");
 
@@ -149,5 +149,32 @@ public class ActServiceImpl implements ActService {
             System.exit(-1);
         }
         return null;
+    }
+
+    @PostConstruct
+    public void init() {
+        try {
+            // A wallet stores a collection of identities
+            Path walletPath = Paths.get(MAGNETOCORP_PATH, "identity", "user", "isabella", "wallet");
+            Wallet wallet = Wallet.createFileSystemWallet(walletPath);
+
+            // Location of credentials to be stored in the wallet
+            Path credentialPath = Paths.get(MAGNETOCORP_PATH, "..", "..", "..", "basic-network", "crypto-config",
+                    "peerOrganizations", "org1.example.com", "users", "User1@org1.example.com", "msp");
+            Path certificatePem = credentialPath.resolve(Paths.get("signcerts",
+                    "User1@org1.example.com-cert.pem"));
+            Path privateKey = credentialPath.resolve(Paths.get("keystore",
+                    "c75bd6911aca808941c3557ee7c97e90f3952e379497dc55eb903f31b50abc83_sk"));
+
+            // Load credentials into wallet
+            String identityLabel = "User1@org1.example.com";
+            Wallet.Identity identity = Wallet.Identity.createIdentity("Org1MSP", Files.newBufferedReader(certificatePem), Files.newBufferedReader(privateKey));
+
+            wallet.put(identityLabel, identity);
+
+        } catch (IOException e) {
+            System.err.println("Error adding to wallet");
+            e.printStackTrace();
+        }
     }
 }
